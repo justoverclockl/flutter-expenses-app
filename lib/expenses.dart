7da54af4503e0fa2d59e6535241fc3321fe40c2c
Marco Colia <marco.colia@fiftydeg.com>
2023-12-01 16:00:43 +0100

    added first placeholder

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -10,6 +10,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('chart'),
+          Text('list of expenses'),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:expense_app/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ,
+    home: Expenses(),
   ));
 }
