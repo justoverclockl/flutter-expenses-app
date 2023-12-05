399bcc6379a0c811fe1ea89b5b7c268b8fe4b749
Marco Colia <marco.colia@fiftydeg.com>
2023-12-05 17:24:13 +0100

    best method to style our app

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:expense_app/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 220, 189, 252)),
     debugShowCheckedModeBanner: false,
-    home: Expenses(),
+    home: const Expenses(),
   ));
 }
