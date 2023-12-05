8d254ac29dd598abd4e6433208e0ba4cd3315fb0
Marco Colia <marco.colia@fiftydeg.com>
2023-12-05 16:53:50 +0100

    add isScrollControlled to make sure that the modal fits content

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -84,6 +84,9 @@ class _NewExpenseState extends State<NewExpense> {
       date: _selectedDate!,
       category: _selectedCategory,
     ));
+
+    // close the modal after submission
+    Navigator.pop(context);
   }
 
   // on unmount this controller is not needed anymore
@@ -97,7 +100,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
