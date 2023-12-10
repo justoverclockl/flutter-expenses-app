6aebe80e39de1cc5ec9af4ed177b2877d0347c7c
Marco Colia <marco.colia@fiftydeg.com>
2023-12-10 22:50:13 +0100

    add dark theme support and code

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,28 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(MaterialApp(
+    // dark theme
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+      ),
+    ),
+    // light theme
     theme: ThemeData().copyWith(
       colorScheme: kColorScheme,
       appBarTheme: const AppBarTheme().copyWith(
@@ -31,6 +51,7 @@ void main() {
           ),
     ),
     debugShowCheckedModeBanner: false,
+    themeMode: ThemeMode.system, // this is a default
     home: const Expenses(),
   ));
 }
